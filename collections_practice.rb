76abca01d3d51d91d2c10b106b02f95228b1d0cd
apocalypse575 <apocalypse575@gmail.com>
2017-12-17 15:13:26 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,20 @@
-# your code goes here
+# def begins_with_r(array)
+#   i = 0
+#   while i < array.length
+#   if array.select { |r| r.start_with?('r') }
+#     puts true
+#           i += 1
+#   else
+#     puts false
+#           i += 1
+#   end
+# end
+# end
+
+collection2.all? {|word| word.start_with?("r")}
+
+
+
+#begins_with_r Return true if every element of the tools array starts with an "r" and false otherwise.
+
+#begins_with_r should return false if there's an element that does not begin with r
